f42106958528c04aeeca4901d509a767a2c6654a
Adam Nord <adam.nord@us.af.mil>
2026-03-23 12:51:46 -0700

    updated index.ts for DATABASE_RUL

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,18 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+let _db: ReturnType<typeof drizzle<typeof schema>> | undefined;
 
-const client = postgres(env.DATABASE_URL);
+function getDb() {
+	if (!_db) {
+		if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+		_db = drizzle(postgres(env.DATABASE_URL), { schema });
+	}
+	return _db;
+}
 
-export const db = drizzle(client, { schema });
+export const db = new Proxy({} as ReturnType<typeof getDb>, {
+	get(_, prop) {
+		return getDb()[prop as keyof ReturnType<typeof getDb>];
+	}
+});
